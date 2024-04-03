a72c672c96f19888a4c673e72ce540247dfbb14e
Paul Jankowski <8BitTitan@gmail.com>
2024-04-03 01:26:09 -0400

    Remove logout route that is no longer used

--- apps/web/app/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { logout } from '~/lib/api'
-
-export const POST = async () => {
-  const data = await logout()
-
-  return new Response(JSON.stringify(data), {
-    status: data.success ? 200 : 400,
-  })
-}
