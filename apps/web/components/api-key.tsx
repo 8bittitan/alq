673e4f8f1f92fe32d7960f3d9bd7540c9a1af5c6
Paul Jankowski <8BitTitan@gmail.com>
2024-04-05 09:07:00 -0400

    Add confirmation before rerolling api key

--- apps/web/components/api-key.tsx
+++ apps/web/components/api-key.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Copy, MoreVertical, RotateCw } from 'lucide-react'
+import { useState } from 'react'
 import { toast } from 'sonner'
 
 import { rollApiKey } from '~/actions/apikey'
@@ -13,14 +14,31 @@ import {
 } from '~/components/ui/dropdown-menu'
 import { maskApiKey } from '~/lib/apikey'
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './ui/dialog'
+import { Input } from './ui/input'
+
 export default function ApiKey({ apikey }: { apikey: string }) {
+  const [rerollConfirmOk, setRerollConfirmOk] = useState(false)
+  const [rerollConfirmDialogOpen, setRerollConfirmDialogOpen] = useState(false)
+
   const handleCopy = async () => {
     await navigator.clipboard.writeText(apikey)
     toast.success('API Key copied to clipboard')
   }
 
   const handleRerollApiKey = async () => {
+    if (!rerollConfirmOk) return
+
     await rollApiKey()
+
+    setRerollConfirmDialogOpen(false)
 
     toast.success('API Key has been rolled')
   }
@@ -40,13 +58,51 @@ export default function ApiKey({ apikey }: { apikey: string }) {
               <Copy className="h-4 w-4 mr-2" />
               Copy
             </DropdownMenuItem>
-            <DropdownMenuItem onClick={handleRerollApiKey}>
+            <DropdownMenuItem onClick={() => setRerollConfirmDialogOpen(true)}>
               <RotateCw className="h-4 w-4 mr-2" />
               Reroll
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
+      <Dialog
+        open={rerollConfirmDialogOpen}
+        onOpenChange={(open) => setRerollConfirmDialogOpen(open)}
+      >
+        <DialogContent className="sm:max-w-2xl">
+          <DialogHeader>
+            <DialogTitle>Warning</DialogTitle>
+            <DialogDescription>This is a destructive action.</DialogDescription>
+          </DialogHeader>
+          <div>
+            <p className="mb-4">
+              Rerolling your API Key means any current requests using your
+              current key will stop working. To make sure you understand this,
+              please type the word <strong>confirm</strong> below before making
+              this action.
+            </p>
+            <div className="max-w-56">
+              <Input
+                onChange={(e) =>
+                  setRerollConfirmOk(
+                    e.currentTarget.value.toLowerCase() === 'confirm',
+                  )
+                }
+              />
+            </div>
+          </div>
+
+          <DialogFooter>
+            <Button
+              variant="destructive"
+              disabled={!rerollConfirmOk}
+              onClick={handleRerollApiKey}
+            >
+              Confirm reroll
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   )
 }
